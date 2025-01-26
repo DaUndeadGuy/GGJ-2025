ef79e4d081a75dfb001a6254132f403fc5349f7f
Kaci Reenea Santos <107488670+KR-GS@users.noreply.github.com>
2025-01-26 08:04:39 +0800

    Added working start screen

---
+++ Assets/Scripts/StartScene.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScene : MonoBehaviour
+{
+    public void NextScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
